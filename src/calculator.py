0e495e64868308055e8d83b275de03e35b35f1bf
es372 <es372@njit.edu>
2021-06-23 13:21:37 -0400

    Feature: Unit Test 2

--- src/calculator.py
+++ src/calculator.py
@@ -5,3 +5,15 @@ class Calculator:
     def __init__(self):
         pass
 
+    @staticmethod
+    def addition(a,b):
+        c = a + b
+        return c
+
+    def add(self, a, b):
+        self.result = self.addition(a,b)
+        return self.result
+
+
+
+
--- src/calculatorTests.py
+++ src/calculatorTests.py
@@ -3,10 +3,16 @@ from calculator import Calculator
 
 class CalculatorTestCase(unittest.TestCase):
 
+    def setUp(self) -> None:
+        self.Calculator = Calculator()
+
     def test_instantiate_calculator(self):
         system = Calculator()
         self.assertIsInstance(system, Calculator)
 
+    def test_addMethod(self):
+        self.assertEqual(self.Calculator.add(2, 2), 4)
+        self.assertEqual(self.Calculator.result, 4)
 
 
 
