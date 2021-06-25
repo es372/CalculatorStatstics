37ee74ceeda9b0ebbd238ca299688ed0fb46a6f9
es372 <es372@njit.edu>
2021-06-24 20:15:50 -0400

    Feature: CSV Test Multiplication

--- src/csvTests.py
+++ src/csvTests.py
@@ -18,6 +18,10 @@ class CalculatorCsvTest(unittest.TestCase):
         for values in self.csv_reader.int_values:
             self.assertEqual(self.Calculator.add(values['a'], values['b']), values['result'])
 
+    def test_csvReader_multiplication(self):
+        self.csv_reader = CsvReader('./src/multiplication.csv')
+        for values in self.csv_reader.int_values:
+            self.assertEqual(self.Calculator.add(values['a'], values['b']), values['result'])
 
 if __name__ == '__main__':
     unittest.main()
