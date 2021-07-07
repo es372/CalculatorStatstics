9185cc5182fc968ca780ad1fac5d7d8bdc499886
es372 <es372@njit.edu>
2021-07-07 17:23:39 -0400

    Error: Import Error Attempt to Fix Python Path

---
+++ Calculator/__init__.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+current_path = os.path.abspath(os.getcwd())
+sys.path.append(current_path)
+
+
+
---
+++ Statistics/__init__.py
@@ -0,0 +1,5 @@
+import os
+import sys
+
+current_path = os.path.abspath(os.getcwd())
+sys.path.append(current_path)
