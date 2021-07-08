4c4feda9402f42390b5a21ba4f719266388cc448
es372 <es372@njit.edu>
2021-07-08 00:15:02 -0400

    Error: test new dataset

--- Statistics/mean.py
+++ Statistics/mean.py
@@ -3,5 +3,5 @@ from Calculator.division import division
 def mean(list_of_data):
     total = sum(list_of_data)
     sample_size = len(list_of_data)
-    return round(division(total, sample_size), 2)
+    return division(total, sample_size)
 
