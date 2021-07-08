5301bbe78b72f0e0fcd8c47bda21df1b4faa2cf8
es372 <es372@njit.edu>
2021-07-08 00:17:57 -0400

    Error: test new mean method

--- Statistics/mean.py
+++ Statistics/mean.py
@@ -1,7 +1,5 @@
 from Calculator.division import division
 
 def mean(list_of_data):
-    total = sum(list_of_data)
-    sample_size = len(list_of_data)
-    return division(total, sample_size)
+    return division(sum(list_of_data), len(list_of_data))
 
